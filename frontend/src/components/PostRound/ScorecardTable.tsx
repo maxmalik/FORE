12f86daeb760213c1455b5a2167993724c2f3271
Max Malik <maxzmalik@gmail.com>
2025-01-31 18:12:43 -0500

    More refactoring

---
+++ frontend/src/components/PostRound/ScorecardTable.tsx
@@ -0,0 +1,106 @@
+import Form from "react-bootstrap/Form";
+import Table from "react-bootstrap/Table";
+
+import { Course } from "../../utils/courses";
+
+interface ScorecardTableProps {
+  course: Course;
+  teeBoxIndex: number;
+  scores: Record<string, string>;
+  onScoreChange: (holeNumber: number, value: string) => void;
+}
+
+// Returns a list of numbers from 1 (inclusive) to n (inclusive)
+function range(n: number): number[] {
+  return Array.from({ length: n }, (_, i) => i + 1);
+}
+
+function ScorecardTable({
+  course,
+  teeBoxIndex,
+  scores,
+  onScoreChange,
+}: ScorecardTableProps) {
+  return (
+    <Table responsive bordered size="sm">
+      <thead>
+        <tr>
+          <th className="align-middle">Hole</th>
+          {range(course.num_holes).map((holeNumber) => (
+            <th key={holeNumber} className="text-center align-middle">
+              {holeNumber}
+            </th>
+          ))}
+          <th className="text-center align-middle px-2">Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        {course.tee_boxes.length > 0 && (
+          <tr>
+            <td className="align-middle">Par</td>
+            {course.scorecard.map((hole) => (
+              <td key={hole.Hole} className="text-center align-middle">
+                {hole.Par}
+              </td>
+            ))}
+            <td className="text-center align-middle">
+              {course.scorecard.reduce((acc, hole) => acc + hole.Par, 0)}
+            </td>
+          </tr>
+        )}
+
+        {course.tee_boxes.length > 0 && (
+          <tr>
+            <td className="align-middle">
+              {course.tee_boxes[teeBoxIndex].tee}{" "}
+              {course.length_format === "Y" ? "(Yards)" : "(Meters)"}
+            </td>
+            {course.scorecard.map((hole) => (
+              <td key={hole.Hole} className="text-center align-middle">
+                {hole.tees[`teeBox${teeBoxIndex + 1}`].yards}
+              </td>
+            ))}
+            <td className="text-center align-middle">
+              {course.tee_boxes[teeBoxIndex].total_yards}
+            </td>
+          </tr>
+        )}
+        {course.tee_boxes.length > 0 && (
+          <tr>
+            <td className="align-middle">Handicap</td>
+            {course.scorecard.map((hole, index) => (
+              <td key={index + 1} className="text-center">
+                {hole.Handicap}
+              </td>
+            ))}
+            <td></td>
+          </tr>
+        )}
+        <tr className="table-active">
+          <td className="align-middle">Score</td>
+          {range(course.num_holes).map((holeNumber) => (
+            <td
+              key={holeNumber}
+              style={{ width: "40px" }}
+              className="text-center align-middle"
+            >
+              <Form.Control
+                min="0"
+                value={scores[holeNumber.toString()]}
+                onChange={(e) => onScoreChange(holeNumber, e.target.value)}
+                className="p-1 text-center"
+              />
+            </td>
+          ))}
+          <td className="text-center align-middle">
+            {Object.values(scores).reduce(
+              (acc, score) => acc + Number(score),
+              0
+            )}
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+export default ScorecardTable;
--- frontend/src/pages/PostRound.tsx
+++ frontend/src/pages/PostRound.tsx
@@ -1,23 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
-import Dropdown from "react-bootstrap/Dropdown";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import Modal from "react-bootstrap/Modal";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Table from "react-bootstrap/Table";
-import Tooltip from "react-bootstrap/Tooltip";
-import { IoMdClose } from "react-icons/io";
 
-import CourseSearchbar from "../components/CourseSearchbar";
 import ForeNavbar from "../components/ForeNavbar";
 import ClearCourseSelectionButton from "../components/PostRound/ClearCourseSelectionButton";
 import CourseCard from "../components/PostRound/CourseCard";
 import ResultsDropdown from "../components/PostRound/ResultsDropdown";
+import ScorecardTable from "../components/PostRound/ScorecardTable";
 import TeeBoxSelectionModal from "../components/PostRound/TeeBoxSelectionModal";
-import { callSearchApi, Course, Hole, TeeBox } from "../utils/courses";
+import { callSearchApi, Course } from "../utils/courses";
 
 function PostRound() {
   const [selectedResultId, setSelectedResultId] = useState<string | null>(null);
@@ -108,25 +102,16 @@ function PostRound() {
     const selectedResult = results.find(
       (result) => result._id === selectedResultId
     )!;
-    for (let i = 1; i <= selectedResult.num_holes; i++) {
-      setScores((prevScores) => ({
-        ...prevScores,
-        [i.toString()]: "",
-      }));
-    }
-  }
-
-  // Returns a list of numbers from 1 (inclusive) to n (inclusive)
-  function range(n: number): number[] {
-    return Array.from({ length: n }, (_, i) => i + 1);
+    setScores(getInitialScores(selectedResult.num_holes));
   }
 
   return (
     <>
       <ForeNavbar pageName="Post Round" />
-      <Container className="my-5 col-sm-7 col-md-5 col-lg-4 col-xxl-3">
-        <h1 className="mb-4 text-center">Post Round</h1>
-        {!selectedResultId ? (
+      <h1 className="my-5 text-center">Post Round</h1>
+      {!selectedResultId ? (
+        // If no result is selected, display the search bar and dropdown
+        <Container className="my-5 col-sm-7 col-md-5 col-lg-4 col-xxl-3">
           <Form.Control
             type="text"
             placeholder="Search for a course..."
@@ -136,168 +121,67 @@ function PostRound() {
             onFocus={() => results.length > 0 && setShowDropdown(true)}
             onBlur={() => results.length > 0 && setShowDropdown(false)}
           />
-        ) : (
-          <InputGroup>
-            <Card.Text className="form-control readonly-text m-0">
-              <CourseCard
-                course={
-                  results.find((result) => result._id === selectedResultId)!
-                }
-              />
-            </Card.Text>
-            <ClearCourseSelectionButton onClick={handleClearSelection} />
-          </InputGroup>
-        )}
-        <ResultsDropdown
-          show={showDropdown && results.length > 0}
-          results={results}
-          showingResultsFor={submittedSearchTerm}
-          onSelectResult={handleSelectResult}
-        />
-        {selectedResultId && (
-          <TeeBoxSelectionModal
-            show={showTeeBoxSelectionModal}
-            course={results.find((result) => result._id === selectedResultId)!}
-            onTeeBoxSelection={handleTeeBoxSelection}
+          <ResultsDropdown
+            show={showDropdown && results.length > 0}
+            results={results}
+            showingResultsFor={submittedSearchTerm}
+            onSelectResult={handleSelectResult}
           />
-        )}
-      </Container>
-      <Container className="my-5 col-sm-12 col-md-12 col-lg-10 col-xl-8 col-xxl-6">
-        {selectedResultId && selectedTeeBoxIndex !== null && (
-          <Form onSubmit={handleSubmit}>
-            <h5> Add a caption (optional)</h5>
-            <Form.Group className="mb-3">
-              <Form.Control
-                placeholder="Caption"
-                onChange={(e) => setCaption(e.target.value)}
-              />
-            </Form.Group>
+        </Container>
+      ) : (
+        // Otherwise display the selected course and the form
 
-            <h5>Scorecard</h5>
-            <Table responsive bordered size="sm">
-              <thead>
-                <tr>
-                  <th className="align-middle">Hole</th>
-                  {range(
+        <>
+          <Container className="my-5 col-sm-7 col-md-5 col-lg-4 col-xxl-3">
+            <InputGroup>
+              <Card.Text className="form-control readonly-text m-0">
+                <CourseCard
+                  course={
                     results.find((result) => result._id === selectedResultId)!
-                      .num_holes
-                  ).map((holeNumber) => (
-                    <th key={holeNumber} className="text-center align-middle">
-                      {holeNumber}
-                    </th>
-                  ))}
-                  <th className="text-center align-middle px-2">Total</th>
-                </tr>
-              </thead>
-              <tbody>
-                {results.find((result) => result._id === selectedResultId)!
-                  .tee_boxes.length > 0 && (
-                  <tr>
-                    <td className="align-middle">Par</td>
-                    {results
-                      .find((result) => result._id === selectedResultId)!
-                      .scorecard.map((hole) => (
-                        <td
-                          key={hole.Hole}
-                          className="text-center align-middle"
-                        >
-                          {hole.Par}
-                        </td>
-                      ))}
-                    <td className="text-center align-middle">
-                      {results
-                        .find((result) => result._id === selectedResultId)!
-                        .scorecard.reduce((acc, hole) => acc + hole.Par, 0)}
-                    </td>
-                  </tr>
-                )}
+                  }
+                />
+              </Card.Text>
+              <ClearCourseSelectionButton onClick={handleClearSelection} />
+            </InputGroup>
+          </Container>
+          <Container className="my-5 col-sm-12 col-md-12 col-lg-10 col-xl-8 col-xxl-6">
+            <TeeBoxSelectionModal
+              show={showTeeBoxSelectionModal}
+              course={
+                results.find((result) => result._id === selectedResultId)!
+              }
+              onTeeBoxSelection={handleTeeBoxSelection}
+            />
 
-                {results.find((result) => result._id === selectedResultId)!
-                  .tee_boxes.length > 0 && (
-                  <tr>
-                    <td className="align-middle">
-                      {
-                        results.find(
-                          (result) => result._id === selectedResultId
-                        )!.tee_boxes[selectedTeeBoxIndex].tee
-                      }{" "}
-                      {results.find(
-                        (result) => result._id === selectedResultId
-                      )!.length_format === "Y"
-                        ? "(Yards)"
-                        : "(Meters)"}
-                    </td>
-                    {results
-                      .find((result) => result._id === selectedResultId)!
-                      .scorecard.map((hole) => (
-                        <td
-                          key={hole.Hole}
-                          className="text-center align-middle"
-                        >
-                          {hole.tees[`teeBox${selectedTeeBoxIndex + 1}`].yards}
-                        </td>
-                      ))}
-                    <td className="text-center align-middle">
-                      {
-                        results.find(
-                          (result) => result._id === selectedResultId
-                        )!.tee_boxes[selectedTeeBoxIndex].total_yards
-                      }
-                    </td>
-                  </tr>
-                )}
-                {results.find((result) => result._id === selectedResultId)!
-                  .tee_boxes.length > 0 && (
-                  <tr>
-                    <td className="align-middle">Handicap</td>
-                    {results
-                      .find((result) => result._id === selectedResultId)!
-                      .scorecard.map((hole, index) => (
-                        <td key={index + 1} className="text-center">
-                          {hole.Handicap}
-                        </td>
-                      ))}
-                    <td></td>
-                  </tr>
-                )}
-                <tr className="table-active">
-                  <td className="align-middle">Score</td>
-                  {range(
+            {selectedTeeBoxIndex !== null && (
+              <Form>
+                <h5> Add a caption (optional)</h5>
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    placeholder="Caption"
+                    onChange={(e) => setCaption(e.target.value)}
+                  />
+                </Form.Group>
+
+                <h5>Scorecard</h5>
+                <ScorecardTable
+                  course={
                     results.find((result) => result._id === selectedResultId)!
-                      .num_holes
-                  ).map((holeNumber) => (
-                    <td
-                      key={holeNumber}
-                      style={{ width: "40px" }}
-                      className="text-center align-middle"
-                    >
-                      <Form.Control
-                        min="0"
-                        value={scores[holeNumber.toString()]}
-                        onChange={(e) =>
-                          handleScoreChange(holeNumber, e.target.value)
-                        }
-                        className="p-1"
-                      />
-                    </td>
-                  ))}
-                  <td className="text-center align-middle">
-                    {Object.values(scores).reduce(
-                      (acc, score) => acc + Number(score),
-                      0
-                    )}
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-            <div className="text-end">
-              <Button variant="success" onClick={handleSubmit}>
-                Post
-              </Button>
-            </div>
-          </Form>
-        )}
-      </Container>
+                  }
+                  teeBoxIndex={selectedTeeBoxIndex}
+                  scores={scores}
+                  onScoreChange={handleScoreChange}
+                />
+                <div className="text-end">
+                  <Button variant="success" onClick={handleSubmit}>
+                    Post
+                  </Button>
+                </div>
+              </Form>
+            )}
+          </Container>
+        </>
+      )}
     </>
   );
 }
